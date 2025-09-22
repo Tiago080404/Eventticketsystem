2540f467fb3ea066faf655a9da45056f874f22f8
Tiago Antolagic Duarte <tiago.antolagic-duarte@telefonica.com>
2025-09-22 15:28:11 +0200

    neuer try

--- src/main/java/com/eventticketsystem/eventticketsystem/Repository/EventsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventticketsystem.eventticketsystem.Repository;
-
-import com.eventticketsystem.eventticketsystem.Entity.Events;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventsRepository extends JpaRepository<Events,String> {
-}
